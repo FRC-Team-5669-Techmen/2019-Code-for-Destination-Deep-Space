00eeccbbdfe0cc2e3e4f4f58ea56360f86b516c3
IDEA Computer Lab <idea_lab@boscotech.edu>
2019-01-12 14:05:47 -0800

    Moved commands to the generic FRC package.

---
+++ src/main/java/edu/boscotech/frc/commands/ManualMecanumDrive.java
@@ -0,0 +1,90 @@
+package edu.boscotech.frc.commands;
+
+import edu.boscotech.frc.subsystems.MecanumDriveSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ManualMecanumDrive extends Command {
+  private Joystick m_mainStick = new Joystick(0), 
+    m_throttle = new Joystick(1);
+  private double m_xThrottle = 1.0, m_yThrottle = 1.0, m_rThrottle = 0.6;
+  private double m_throttleCurve = 2.0, m_turningThrottleCurve = 2.0;
+  private MecanumDriveSubsystem m_drive;
+
+  public ManualMecanumDrive(MecanumDriveSubsystem drive) {
+    requires(drive);
+    m_drive = drive;
+  }
+
+  /**
+   * Sets the maximum speed the robot can move in the X and Y directions.
+   * @param xMax Maximum speed the robot should be capable of in the X
+   *             direction, in the range 0.0 - 1.0. (Default is 1.0)
+   * @param yMax Maximum speed the robot should be capable of in the Y
+   *             direction, in the range 0.0 - 1.0. (Default is 1.0)
+   */
+  public void setMaxSpeedXY(double xMax, double yMax) {
+    m_xThrottle = xMax;
+    m_yThrottle = yMax;
+  }
+
+  /**
+   * Sets the maximum speed the robot can drive the wheels at to rotate around
+   * the Z axis.
+   * @param rMax Maximum speed the robot can apply to its wheels to rotate, in
+   *             the range 0.0 - 1.0. (Default is 0.6)
+   */
+  public void setMaxSpeedR(double rMax) {
+    m_rThrottle = rMax;
+  }
+
+  /**
+   * Sets the steepness of the curve used to translate the throttle's position
+   * into a value to limit the robot's speed by. For example, a slope of 1 will
+   * give a direct, 1:1 correlation between the throttle and the power given to 
+   * the motors. If the throttle is at 50%, then 50% power will be given to the
+   * motors. If the slope is 2.0, then input values will be squared, so a 50% 
+   * throttle will only give 25% power. This allows for more precision in the 
+   * lower areas of the throttle while still allowing for the full speed of the
+   * robot to be utilized.
+   * @param curve The exponent that should be used to translate throttle
+   *              position into motor power. (Default is 2.0)
+   */
+  public void setThrottleCurve(double curve) {
+    m_throttleCurve = curve;
+  }
+
+  /**
+   * Sets the steepness of the curve used to translate the throttle's position
+   * into a value to limit the robot's rotational speed by. For example, a slope
+   * of 1 will give a direct, 1:1 correlation between the throttle and the power
+   * given to the motors. If the throttle is at 50%, then 50% power will be 
+   * given to the motors. If the slope is 2.0, then input values will be 
+   * squared, so a 50% throttle will only give 25% power. This allows for more 
+   * precision in the lower areas of the throttle while still allowing for the 
+   * full speed of the robot to be utilized.
+   * @param curve The exponent that should be used to translate throttle
+   *              position into motor power. (Default is 2.0)
+   */
+  public void setTurningThrottleCurve(double curve) {
+    m_turningThrottleCurve = curve;
+  }
+
+  @Override
+  protected void execute() {
+    double manualThrottle = Math.pow(m_throttle.getRawAxis(0), m_throttleCurve);
+    double rotationalThrottle = Math.pow(
+      m_throttle.getRawAxis(1), m_turningThrottleCurve
+    );
+    m_drive.driveCartesian(
+      m_mainStick.getX() * m_xThrottle * manualThrottle, 
+      m_mainStick.getY() * m_yThrottle * manualThrottle,
+      m_mainStick.getZ() * m_rThrottle * rotationalThrottle
+    );
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+}
