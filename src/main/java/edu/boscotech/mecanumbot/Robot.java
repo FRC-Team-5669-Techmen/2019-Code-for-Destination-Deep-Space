109894fd63026a68eb8a712511b60850fb9fd457
IDEA Computer Lab <idea_lab@boscotech.edu>
2019-01-15 15:58:32 -0800

    Moved boilerplate code into its own base class.

--- src/main/java/edu/boscotech/mecanumbot/Robot.java
+++ src/main/java/edu/boscotech/mecanumbot/Robot.java
@@ -7,15 +7,7 @@
 
 package edu.boscotech.mecanumbot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import edu.boscotech.mecanumbot.commands.ManualMecanumDrive;
-import edu.boscotech.frc.subsystems.ExampleSubsystem;
+import edu.boscotech.frc.commands.ManualMecanumDrive;
 import edu.boscotech.frc.subsystems.LidarSubsystem;
 import edu.boscotech.frc.subsystems.MecanumDriveSubsystem;
 
@@ -26,119 +18,17 @@ import edu.boscotech.frc.subsystems.MecanumDriveSubsystem;
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+public class Robot extends edu.boscotech.frc.Robot {
   public static MecanumDriveSubsystem m_mecanumDrive
     = new MecanumDriveSubsystem();
   public static LidarSubsystem m_lidar = new LidarSubsystem();
 
-  private void setupTeleopCommands() {
-    addTeleopCommand(new ManualMecanumDrive());
-  }
-
-  //////////////////////////////////////////////////////////////////////////////
-  // EVERYTHING BELOW THIS IS MORE LOW-LEVEL STUFF THAT DOESN'T NEED TO BE
-  // MODIFIED MUCH. IF YOU WANT TO CHANGE HOW THE ROBOT RUNS, CHANGE IT IN OTHER
-  // CODE OR OTHER FILES.
-  //////////////////////////////////////////////////////////////////////////////
-
-  public static OI m_oi;
-  List<Command> m_teleopCommands = new ArrayList<Command>();
-
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_oi = new OI();
-  }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
-
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
-   */
-  @Override
-  public void disabledInit() {
-    for (Command command : m_teleopCommands) {
-      command.cancel();
-    }
-    m_teleopCommands.clear();
+  public Robot() {
+    super();
   }
 
   @Override
-  public void disabledPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
-   */
-  @Override
-  public void autonomousInit() {
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
-     */
-  }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  private void addTeleopCommand(Command command) {
-    command.start();
-    m_teleopCommands.add(command);
-  }
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    setupTeleopCommands();
-  }
-
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
+  protected void setupTeleopCommands() {
+    addTeleopCommand(new ManualMecanumDrive(m_mecanumDrive));
   }
 }
