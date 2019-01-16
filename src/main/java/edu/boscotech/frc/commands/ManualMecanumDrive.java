8d22e22f0045f7f3e8a20adef8f80b3041558ddc
IDEA Computer Lab <idea_lab@boscotech.edu>
2019-01-15 16:00:05 -0800

    Rewrote manual mecanum drive to use the new configuration system.

--- src/main/java/edu/boscotech/frc/commands/ManualMecanumDrive.java
+++ src/main/java/edu/boscotech/frc/commands/ManualMecanumDrive.java
@@ -1,85 +1,34 @@
 package edu.boscotech.frc.commands;
 
+import edu.boscotech.frc.config.AnalogControl;
+import edu.boscotech.frc.config.Controls;
 import edu.boscotech.frc.subsystems.MecanumDriveSubsystem;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ManualMecanumDrive extends Command {
-  private Joystick m_mainStick = new Joystick(0), 
-    m_throttle = new Joystick(1);
-  private double m_xThrottle = 1.0, m_yThrottle = 1.0, m_rThrottle = 0.6;
-  private double m_throttleCurve = 2.0, m_turningThrottleCurve = 2.0;
   private MecanumDriveSubsystem m_drive;
+  private AnalogControl m_xControl, m_yControl, m_rControl, m_xyThrottle, 
+    m_rThrottle;
 
   public ManualMecanumDrive(MecanumDriveSubsystem drive) {
     requires(drive);
     m_drive = drive;
-  }
 
-  /**
-   * Sets the maximum speed the robot can move in the X and Y directions.
-   * @param xMax Maximum speed the robot should be capable of in the X
-   *             direction, in the range 0.0 - 1.0. (Default is 1.0)
-   * @param yMax Maximum speed the robot should be capable of in the Y
-   *             direction, in the range 0.0 - 1.0. (Default is 1.0)
-   */
-  public void setMaxSpeedXY(double xMax, double yMax) {
-    m_xThrottle = xMax;
-    m_yThrottle = yMax;
-  }
-
-  /**
-   * Sets the maximum speed the robot can drive the wheels at to rotate around
-   * the Z axis.
-   * @param rMax Maximum speed the robot can apply to its wheels to rotate, in
-   *             the range 0.0 - 1.0. (Default is 0.6)
-   */
-  public void setMaxSpeedR(double rMax) {
-    m_rThrottle = rMax;
-  }
-
-  /**
-   * Sets the steepness of the curve used to translate the throttle's position
-   * into a value to limit the robot's speed by. For example, a slope of 1 will
-   * give a direct, 1:1 correlation between the throttle and the power given to 
-   * the motors. If the throttle is at 50%, then 50% power will be given to the
-   * motors. If the slope is 2.0, then input values will be squared, so a 50% 
-   * throttle will only give 25% power. This allows for more precision in the 
-   * lower areas of the throttle while still allowing for the full speed of the
-   * robot to be utilized.
-   * @param curve The exponent that should be used to translate throttle
-   *              position into motor power. (Default is 2.0)
-   */
-  public void setThrottleCurve(double curve) {
-    m_throttleCurve = curve;
-  }
-
-  /**
-   * Sets the steepness of the curve used to translate the throttle's position
-   * into a value to limit the robot's rotational speed by. For example, a slope
-   * of 1 will give a direct, 1:1 correlation between the throttle and the power
-   * given to the motors. If the throttle is at 50%, then 50% power will be 
-   * given to the motors. If the slope is 2.0, then input values will be 
-   * squared, so a 50% throttle will only give 25% power. This allows for more 
-   * precision in the lower areas of the throttle while still allowing for the 
-   * full speed of the robot to be utilized.
-   * @param curve The exponent that should be used to translate throttle
-   *              position into motor power. (Default is 2.0)
-   */
-  public void setTurningThrottleCurve(double curve) {
-    m_turningThrottleCurve = curve;
+    Controls con = Controls.getInstance();
+    m_xControl = con.getAnalogControl("mecanum", "x");
+    m_yControl = con.getAnalogControl("mecanum", "y");
+    m_rControl = con.getAnalogControl("mecanum", "r");
+    m_xyThrottle = con.getAnalogControl("mecanum", "xyThrottle");
+    m_rThrottle = con.getAnalogControl("mecanum", "rThrottle");
   }
 
   @Override
   protected void execute() {
-    double manualThrottle = Math.pow(m_throttle.getRawAxis(0), m_throttleCurve);
-    double rotationalThrottle = Math.pow(
-      m_throttle.getRawAxis(1), m_turningThrottleCurve
-    );
     m_drive.driveCartesian(
-      m_mainStick.getX() * m_xThrottle * manualThrottle, 
-      m_mainStick.getY() * m_yThrottle * manualThrottle,
-      m_mainStick.getZ() * m_rThrottle * rotationalThrottle
+      m_yControl.getValue() * m_xyThrottle.getValue(),
+      m_xControl.getValue() * m_xyThrottle.getValue(),
+      m_rControl.getValue() * m_rThrottle.getValue()
     );
   }
 
