d76989fff8f9278681f1f08c03335955a0a5e7c0
IDEA Computer Lab <idea_lab@boscotech.edu>
2019-01-19 18:56:06 -0800

    Removed old library code, moving it to a submodule.

--- src/main/java/edu/boscotech/frc/util/DefaultCommandCreator.java
+++
@@ -1,8 +0,0 @@
-package edu.boscotech.frc.util;
-
-import edu.wpi.first.wpilibj.command.Command;
-
-public interface DefaultCommandCreator {
-  public Command createDefaultTeleopCommand();
-  public Command createDefaultTestCommand();
-}
