5022b2c246d59781f19c178c44a66639b712c455
IDEA Computer Lab <idea_lab@boscotech.edu>
2019-02-03 10:59:39 -0800

    Added a sketch to debug a sensor bar over a serial connection.

---
+++ src/main/arduino/light_bar_debug/light_bar_debug.ino
@@ -0,0 +1,128 @@
+#include <Wire.h>
+
+/* I2C address:
+ *   0x11 - Sensor bar 1
+ *   0x12 - Sensor bar 2
+ * I2C register map:
+ *   0x00 - Status register (unused, always 0x00)
+ *   0x01 - Output register (the integer index of the sensor that is brightest.) (0xFF if no output was computed.)
+ *   0x02 - Reserved.
+ *   0x03 - "Serial number" - 0x01 for the first bar, 0x02 for the second.
+ */
+
+// Define 1 or 2 depending on which bar this is.
+#define SENSOR_BAR_1
+
+typedef unsigned int uint;
+
+#ifdef SENSOR_BAR_1
+const byte I2C_SLAVE_ADDRESS = 0x11;
+const byte I2C_SERIAL_NUMBER = 0x01;
+#else
+const byte I2C_SLAVE_ADDRESS = 0x12;
+const byte I2C_SERIAL_NUMBER = 0x02;
+#endif
+
+const byte I2C_DATA_SIZE = 4, NUM_SENSORS = 16;
+const uint SAMPLES_PER_SENSOR = 8;
+const uint OUTPUT_PRECISION = 8; // A value of 8 means 8 times as many output positions as there are sensors.
+const int ANALOG_PIN = A0;
+// Multiplexer pins must be attached as follows (for efficiency reasons):
+// A = 10, B = 11, C = 12, D = 13
+const uint CALIBRATION[NUM_SENSORS] = {
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0
+};
+const bool ACTIVE[NUM_SENSORS] = {
+  1, 1, 1, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0
+};
+byte i2cRegisters[4] = {0x00, 0xFF, 0x00, I2C_SERIAL_NUMBER};
+byte *output = &i2cRegisters[1], *registerPointer = &i2cRegisters[0];
+
+void setup() {
+  Wire.begin(I2C_SLAVE_ADDRESS);
+  Wire.onRequest(handleI2CRequest);
+  Wire.onReceive(handleI2CReceive);
+
+  // Sets the ADC to go 16x as fast without a decrease in accuracy.
+  ADCSRA |= 0b00000100;
+  ADCSRA &= 0b11111100;
+  DDRB |= 0b00111100; // Set ports 10-13 as output ports.
+
+  Serial.begin(115200);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+}
+
+void loop() {
+  uint values[NUM_SENSORS];
+  uint minValue = 65535; // Brightest value.
+  uint maxValue = 0; // Darkest value.
+  for (byte sensorIndex = 0; sensorIndex < NUM_SENSORS; sensorIndex++) {
+    uint value = 0; // Because of how it is wired, lower voltages means brighter light
+    // Write four-bit representation of sensor index to multiplexer.
+    PORTB &= 0b11000011; // Clear ports 10-13.
+    PORTB |= sensorIndex << 2; // Set ports 10-13 to the binary representation of the index.
+    // Poll the analog pin multiple times to get several sensor readings.
+    delay(10);
+    for (uint sample = 0; sample < SAMPLES_PER_SENSOR; sample++) {
+      value += analogRead(ANALOG_PIN);
+    }
+    values[sensorIndex] = value;
+    if (!ACTIVE[sensorIndex]) continue;
+    minValue = (value < minValue) ? value : minValue;
+    maxValue = (value > maxValue) ? value : maxValue;
+    Serial.print(value);
+    Serial.print("\t");
+    Serial.print(digitalRead(5));
+    Serial.print(digitalRead(6));
+    Serial.print("\t");
+  }
+
+  uint range, newMax;
+  range = maxValue - minValue;
+  newMax = minValue + range / 2; // Threshold to cut off mostly dark sensors.
+  range = newMax - minValue;
+  uint totalIndices = 0, totalWeight = 0;
+  // Weighted average of all sensors based on their brightness relative to min and max values.
+  // Sensors that are darker than newMax are not counted.
+  for (uint sensorIndex = 0; sensorIndex < NUM_SENSORS; sensorIndex++) {
+    if (!ACTIVE[sensorIndex]) continue;
+    if (values[sensorIndex] <= maxValue) {
+      uint weight = (maxValue - values[sensorIndex]) / (range / OUTPUT_PRECISION);
+      totalWeight += weight;
+      totalIndices += sensorIndex * weight;
+    }
+  }
+  
+  *output = totalIndices / (totalWeight / OUTPUT_PRECISION);
+  Serial.println();
+  Serial.print(totalIndices);
+  Serial.print("\t");
+  Serial.print(totalWeight);
+  Serial.print("\t");
+  Serial.print(minValue);
+  Serial.print("\t");
+  Serial.print(maxValue);
+  Serial.print("\t");
+  Serial.println(*output);
+
+  delay(100);
+}
+
+void handleI2CRequest() {
+  Wire.write(registerPointer, I2C_DATA_SIZE);
+}
+
+void handleI2CReceive(int bytesReceived) {
+  if (bytesReceived == 0) return;
+  registerPointer = i2cRegisters + Wire.read();
+  if (registerPointer >= i2cRegisters + I2C_DATA_SIZE) {
+    registerPointer = &i2cRegisters[0];
+  }
+  for (uint i = 0; i < bytesReceived - 1; i++) {
+    // Get rid of extra data.
+    Wire.read();
+  }
+}
